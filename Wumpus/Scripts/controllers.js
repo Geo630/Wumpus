003d0cb63c97bb3227a3743ea0f3f6913a23c767
Geo630 <geocamposm@gmail.com>
2014-10-08 18:57:09 -0600

    primer commit
    se crearon los archivos iniciales.

---
+++ Wumpus/Scripts/controllers.js
@@ -0,0 +1,30 @@
+﻿var app = angular.module('wumpus', []);
+
+app.controller('wumpusController', function ($scope) {
+
+    $scope.cantidad = null;
+    $scope.filas = [];
+    
+    $scope.crear = function () {
+        
+        $scope.filas = [];
+        for (var i = 0; i < $scope.cantidad; i++) {
+            $scope.filas.push({
+                columnas: [],
+                x : i
+            });
+            for (var j = 0; j < $scope.cantidad; j++) {
+                $scope.filas[i].columnas.push({ y: j });
+            }
+            
+        };
+        console.log($scope.filas)
+        //if ($scope.cantidad < 5) {
+        //    alert("el tablero no puede de menos de 5 x 5");
+        //    $scope.tabla = [];
+        //};
+    };
+
+   
+
+});
